ee05fed6b0238c36bd8efb8bfcb834aef15cf320
eulbyvan <work.issdq@gmail.com>
2022-10-10 02:30:37 +0700

    add user viewmodel

---
+++ LegoasAssignment/Models/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LegoasAssignment.Models.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        public string Fullname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
